b8e44361aa913dc2c60ff437b04f8c530088e0d6
Priya <priyanka.gupta7679@gmail.com>
2025-05-14 12:49:31 +0530

    Test Push

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Welcome to the Platform!"})
+    return jsonify({"message": "Hi Priyanka"})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
