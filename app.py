33572d3b70438d1abdd0b5a4b4bd81f4d31fddcc
Priyanka Gupta <60427155+priyankagupta7679@users.noreply.github.com>
2025-05-16 15:48:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hi Priyanka"})
+    return jsonify({"message": "Hi Demo1"})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
